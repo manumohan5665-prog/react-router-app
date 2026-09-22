e06e222c4551af7c558aedc8ae848f4c13e5cc61
Manu Mohandas <manumohan5665@gmail.com>
2026-09-22 22:18:53 +0530

    added content to Users.jsx

---
+++ src/pages/Users.jsx
@@ -0,0 +1,10 @@
+function Users() {
+    return (
+        <div>
+            <h1>Users Page</h1>
+            <p>Here we will display our users.</p>
+        </div>
+    );
+}
+
+export default Users;

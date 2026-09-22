7cd604443f7f057fe5694caba55fc89cf46f15e8
Manu Mohandas <manumohan5665@gmail.com>
2026-09-22 22:24:55 +0530

    added content to UserDetail.jsx

---
+++ src/pages/UserDetail.jsx
@@ -0,0 +1,10 @@
+function UserDetail() {
+    return (
+        <div>
+            <h1>User Detail Page</h1>
+            <p>User information will appear here.</p>
+        </div>
+    );
+}
+
+export default UserDetail;

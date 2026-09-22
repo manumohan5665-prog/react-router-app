13becaab4ba729676ab9064d4fb1516fd450cb41
Manu Mohandas <manumohan5665@gmail.com>
2026-09-22 22:14:36 +0530

    added content to About.jsx

---
+++ src/pages/About.jsx
@@ -0,0 +1,10 @@
+function About() {
+    return (
+        <div>
+            <h1>About Page</h1>
+            <p>This application demonstrates React Router.</p>
+        </div>
+    );
+}
+
+export default About;

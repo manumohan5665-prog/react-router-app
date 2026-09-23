7a91f745ba0b1eceb3717375e4f571c553741a45
Manu Mohandas <manumohan5665@gmail.com>
2026-09-23 17:01:56 +0530

    added users array into seperate file-users.js

---
+++ src/data/users.js
@@ -0,0 +1,29 @@
+const users = [
+    {
+        id: 1,
+        name: "John Doe",
+        email: "john@example.com",
+    },
+    {
+        id: 2,
+        name: "Jane Smith",
+        email: "jane@example.com",
+    },
+    {
+        id: 3,
+        name: "Michael Johnson",
+        email: "michael@example.com",
+    },
+    {
+        id: 4,
+        name: "Emily Davis",
+        email: "emily@example.com",
+    },
+    {
+        id: 5,
+        name: "David Wilson",
+        email: "david@example.com",
+    }
+];
+
+export default users;
--- src/pages/UserDetail.jsx
+++ src/pages/UserDetail.jsx
@@ -1,27 +1,5 @@
 import { Link, useParams } from "react-router-dom";
-
-const users = [
-    {
-        id: 1,
-        name: "John Doe",
-        email: "john@example.com",
-    },
-    {
-        id: 2,
-        name: "Jane Smith",
-        email: "jane@example.com",
-    },
-    {
-        id: 3,
-        name: "Michael Johnson",
-        email: "michael@example.com",
-    },
-    {
-        id: 4,
-        name: "Emily Davis",
-        email: "emily@example.com",
-    },
-];
+import users from "../data/users";
 
 function UserDetail() {
 
